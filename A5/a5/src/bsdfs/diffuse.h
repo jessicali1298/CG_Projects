ec283ad2ae346be1d4cf744f586de0308fa4229f
Jessica Li <yihui.li@mail.mcgill.ca>
2018-11-10 17:59:19 -0500

    A5 1.1 done??

---
+++ A5/a5/src/bsdfs/diffuse.h
@@ -0,0 +1,76 @@
+/*
+    This file is part of TinyRender, an educative rendering system.
+
+    Designed for ECSE 446/546 Realistic/Advanced Image Synthesis.
+    Derek Nowrouzezahrai, McGill University.
+*/
+
+#pragma once
+
+#include "core/core.h"
+
+TR_NAMESPACE_BEGIN
+
+/**
+ * Perfectly diffuse, Lambertian reflectance model
+ */
+struct DiffuseBSDF : BSDF {
+    std::unique_ptr<Texture < v3f>> albedo;
+
+    DiffuseBSDF(const WorldData& scene, const Config& config, const size_t& matID) : BSDF(scene, config, matID) {
+        const tinyobj::material_t& mat = scene.materials[matID];
+
+        if (mat.diffuse_texname.empty())
+            albedo = std::unique_ptr<Texture<v3f>>(new ConstantTexture3f(glm::make_vec3(mat.diffuse)));
+        else
+            albedo = std::unique_ptr<Texture<v3f>>(new BitmapTexture3f(config, mat.diffuse_texname));
+
+        components.push_back(EDiffuseReflection);
+
+        combinedType = 0;
+        for (size_t i = 0; i < components.size(); ++i)
+            combinedType |= components[i];
+    }
+
+    v3f eval(const SurfaceInteraction& i) const override {
+        v3f val(0.f);
+        // TODO: Add previous assignment code (if needed)
+        //1. Check that the incoming and outgoing rays are headed in the correct directions; if not return black.
+        //2. Otherwise return the evaluated albedo divided by π, and multiplied by the cosine factor cosθi.
+        if (Frame::cosTheta(i.wi) >= 0.f  && Frame::cosTheta(i.wo) >= 0.f) {
+            v3f diffuseBRDF = (albedo->eval(worldData, i))/M_PI;
+            val = diffuseBRDF;
+        }
+
+        return val;
+    }
+
+    float pdf(const SurfaceInteraction& i) const override {
+        float pdf = 0.f;
+        // TODO: Implement this
+
+        pdf = Warp::squareToCosineHemispherePdf(i.wi);
+        return pdf;
+    }
+
+    v3f sample(SurfaceInteraction& i, const v2f& sample, float* pdf) const override {
+        v3f val(0.f);
+        // TODO: Implement this
+        //Cosine Hemisphere
+
+        v3f sampleDir, sampleDir_world;
+        float cosThetai;
+
+        i.wi = Warp::squareToCosineHemisphere(sample);
+        //sampleDir_world = glm::normalize(i.frameNs.toWorld(i.wi));
+
+        *pdf = this->pdf(i);
+        val = this->eval(i);
+
+        return val;
+    }
+
+    std::string toString() const override { return "Diffuse"; }
+};
+
+TR_NAMESPACE_END
---
+++ A5/a5/src/bsdfs/phong.h
@@ -0,0 +1,119 @@
+/*
+    This file is part of TinyRender, an educative rendering system.
+
+    Designed for ECSE 446/546 Realistic/Advanced Image Synthesis.
+    Derek Nowrouzezahrai, McGill University.
+*/
+
+#pragma once
+
+#include "core/core.h"
+
+TR_NAMESPACE_BEGIN
+
+/**
+ * Modified Phong reflectance model
+ */
+struct PhongBSDF : BSDF {
+
+    std::unique_ptr<Texture < v3f>> specularReflectance;
+    std::unique_ptr<Texture < v3f>> diffuseReflectance;
+    std::unique_ptr<Texture < float>> exponent;
+    float specularSamplingWeight;
+    float scale;
+
+    PhongBSDF(const WorldData& scene, const Config& config, const size_t& matID) : BSDF(scene, config, matID) {
+        const tinyobj::material_t& mat = scene.materials[matID];
+
+        if (mat.specular_texname.empty())
+            specularReflectance = std::unique_ptr<Texture<v3f>>(new ConstantTexture3f(glm::make_vec3(mat.specular)));
+        else
+            specularReflectance = std::unique_ptr<Texture<v3f>>(new BitmapTexture3f(config, mat.specular_texname));
+
+        if (mat.diffuse_texname.empty())
+            diffuseReflectance = std::unique_ptr<Texture<v3f>>(new ConstantTexture3f(glm::make_vec3(mat.diffuse)));
+        else
+            diffuseReflectance = std::unique_ptr<Texture<v3f>>(new BitmapTexture3f(config, mat.diffuse_texname));
+
+        exponent = std::unique_ptr<Texture<float>>(new ConstantTexture1f(mat.shininess));
+
+        //get scale value to ensure energy conservation
+        v3f maxValue = specularReflectance->getMax() + diffuseReflectance->getMax();
+        float actualMax = max(max(maxValue.x, maxValue.y), maxValue.z);
+        scale = actualMax > 1.0f ? 0.99f * (1.0f / actualMax) : 1.0f;
+
+        float dAvg = getLuminance(diffuseReflectance->getAverage() * scale);
+        float sAvg = getLuminance(specularReflectance->getAverage() * scale);
+        specularSamplingWeight = sAvg / (dAvg + sAvg);
+
+        components.push_back(EGlossyReflection);
+        components.push_back(EDiffuseReflection);
+
+        combinedType = 0;
+        for (unsigned int component : components)
+            combinedType |= component;
+    }
+
+    inline v3f reflect(const v3f& d) const {
+        return v3f(-d.x, -d.y, d.z);
+    }
+
+    v3f eval(const SurfaceInteraction& i) const override {
+        v3f val(0.f);
+        // TODO: Add previous assignment code (if needed)
+        //1. Check that the incoming ray is not hitting a backface (just like diffuse).
+        //2. Evaluate the Phong BRDF using Equation (1).
+        //3. Return this value multiplied by the cosine factor.
+
+        v3f specDirection = reflect(i.wi);
+        v3f diffuseReflect = diffuseReflectance->eval(worldData, i);
+        v3f specReflect = specularReflectance->eval(worldData, i);
+        float exp = exponent->eval(worldData, i);
+        float cosAlpha = std::fmax(0.f,glm::dot(i.wo,specDirection)/glm::length(i.wo)/glm::length(specDirection));
+
+        if (Frame::cosTheta(i.wi) >= 0 && Frame::cosTheta(i.wo) >= 0 ) {
+            v3f phongBRDF = (diffuseReflect/M_PI) + (specReflect*((exp+2)/(2*M_PI))*glm::pow(cosAlpha, exp));
+            val = scale * phongBRDF;
+
+        }
+        return val;
+    }
+
+    float pdf(const SurfaceInteraction& i) const override {
+        float pdf = 0.f;
+        // TODO: Implement this
+        float exp = exponent->eval(worldData, i);
+
+        v3f viewReflect_world = glm::normalize(i.frameNs.toWorld(reflect(i.wo)));
+        Frame phongFrame(viewReflect_world);
+
+        v3f sample = phongFrame.toLocal(i.frameNs.toWorld(i.wi));
+        pdf = Warp::squareToPhongLobePdf(sample,exp);
+
+        return pdf;
+    }
+
+    v3f sample(SurfaceInteraction& i, const v2f& _sample, float* pdf) const override {
+        v3f val(0.f);
+        // TODO: Implement this
+        v3f viewReflect_world;
+        v3f sampleDir, sampleDir_world;
+        float exp = exponent->eval(worldData, i);
+
+        viewReflect_world = glm::normalize(i.frameNs.toWorld(reflect(i.wo)));
+        Frame newFrame(viewReflect_world);
+
+
+        sampleDir = Warp::squareToPhongLobe(_sample, exp);
+
+        //i.wi now in local space of the shading point
+        i.wi = glm::normalize(i.frameNs.toLocal(newFrame.toWorld(sampleDir)));
+        *pdf = this->pdf(i);
+        val = this->eval(i);
+        return val;
+    }
+
+    std::string toString() const override { return "Phong"; }
+};
+
+TR_NAMESPACE_END
